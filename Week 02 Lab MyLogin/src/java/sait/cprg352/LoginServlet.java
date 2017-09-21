bc6451f1bcf7547dfad15d7138b8cbeaa1f3c1a3
636334 <636334@ELF6IT215856.ACDM.DS.SAIT.CA>
2017-09-21 08:15:06 -0600

    Week 02 Lab 2
    create jsp and servlet files

--- Week 02 Lab MyLogin/src/java/sait/cprg352/LoginServlet.java
+++ Week 02 Lab MyLogin/src/java/sait/cprg352/LoginServlet.java
@@ -18,7 +18,50 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LoginServlet extends HttpServlet 
 {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // get the parameters from the URL
+        String ageString = request.getParameter("age");
+       
 
+        // set the JSP request variables to the values first name and last name
+       
+        
+        if (ageString == null || ageString.trim().isEmpty()) 
+        {
+            request.setAttribute("message", "Please enter a valid number");
+
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+            
+            return;
+        }
+        try
+        {
+            int ageNumber = Integer.parseInt(ageString);
+            request.setAttribute("age", ageString);
+            request.setAttribute("message", "Your age next will be "+(ageNumber+1));
+        
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").forward(request, response);
+        }
+        catch (NumberFormatException e)
+        {
+            request.setAttribute("message", "You must enter an integer.");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/ageCalculator.jsp").
+                forward(request, response);
+        }
+        
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+        
+    }
  
 
 }
--- Week 02 Lab MyLogin/web/WEB-INF/login.jsp
+++ Week 02 Lab MyLogin/web/WEB-INF/login.jsp
@@ -9,9 +9,16 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Login Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Login</h1>
+        <div>
+            <form action="login" method="get">
+                Username: <input type="text" name="username" value="${username}">
+                Password: <input type="text" name="password" value="${password}">
+                <input type="submit" value="login">  
+            </form>    
+        </div>
     </body>
 </html>
